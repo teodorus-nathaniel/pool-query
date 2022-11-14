929b4fb91232d8e563a8931d9350890f1531eb3e
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-11-14 18:04:52 +0700

    Revert "Add exports"
    This reverts commit dc4bf9db596126fcdcc7af7822b6ed36bffec195.

--- tools/cleanup.js
+++ tools/cleanup.js
@@ -25,5 +25,5 @@ if (folder) {
   deleteFolderRecursive(Path.join(__dirname, '../dist/cjs'))
   deleteFolderRecursive(Path.join(__dirname, '../dist/esm'))
   deleteFolderRecursive(Path.join(__dirname, '../dist/umd'))
-  deleteFolderRecursive(Path.join(__dirname, '../dist/types'))
+  deleteFolderRecursive(Path.join(__dirname, '../dist'))
 }
